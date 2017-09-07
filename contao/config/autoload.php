eca89ec97c4dbc5167fed0199d2ce6fd59faac17
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-09-07 20:36:06 +0200

    Create widget

---
+++ contao/config/autoload.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of richardhj/contao-textfield-multiple.
+ *
+ * Copyright (c) 2017 Richard Henkenjohann
+ *
+ * @package   FormTextFieldMultiple
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2017 Richard Henkenjohann
+ * @license   https://github.com/MetaModels/attribute_translatedtabletext/blob/master/LICENSE LGPL-3.0
+ */
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(
+    [
+        'form_textfield_multiple' => 'system/modules/textfield_multiple/templates',
+    ]
+);
--- src/FormTextFieldMultiple.php
+++ src/FormTextFieldMultiple.php
@@ -18,4 +18,55 @@ use Contao\FormTextField;
 class FormTextFieldMultiple extends FormTextField
 {
 
+    /**
+     * Template
+     *
+     * @var string
+     */
+    protected $strTemplate = 'form_textfield_multiple';
+
+    /**
+     * Parse the template file and return it as string
+     *
+     * @param array $attributes An optional attributes array
+     *
+     * @return string The template markup
+     */
+    public function parse($attributes = null)
+    {
+        if (!$this->multiple) {
+            $this->strTemplate = 'form_textfield';
+        }
+
+        return parent::parse($attributes);
+    }
+
+    /**
+     * Generate the widget and return it as string
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        if (!$this->multiple) {
+            return parent::generate();
+        }
+
+        $return = '';
+
+        $originalName  = $this->strName;
+        $originalId    = $this->strId;
+        $this->strName = $originalName . '[]';
+
+        for ($i = 0; $i < $this->size; $i++) {
+            $this->strId = $originalId . '_' . $i;
+
+            $return .= parent::generate();
+        }
+
+        $this->strName = $originalName;
+        $this->strId   = $originalId;
+
+        return $return;
+    }
 }
