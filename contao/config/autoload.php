6f3c41d41f2847915209e0a4ea4c1fdfa23a6b12
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-09-21 13:03:50 +0200

    Update PHPDoc

--- contao/config/autoload.php
+++ contao/config/autoload.php
@@ -5,6 +5,7 @@
  *
  * Copyright (c) 2017 Richard Henkenjohann
  *
+ * @package   richardhj/contao-textfield-multiple
  * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @copyright 2017 Richard Henkenjohann
  * @license   https://github.com/richardhj/contao-textfield-multiple/blob/master/LICENSE LGPL-3.0
